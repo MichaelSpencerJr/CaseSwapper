8e109e08bea82d6bb4d075ae87fc58f44ee98dfc
Michael Spencer Jr <Michael.Spencer.Jr@gmail.com>
2020-08-02 20:04:04 -0500

    Initial commit of text case swapper.  Did extremely minor bench testing, but the program doesn't do a lot so it should be OK.  (Famous last words I know.)

---
+++ Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace CaseSwapper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.btnPaste = new System.Windows.Forms.Button();
+            this.btnSwap = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 41);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(776, 397);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(12, 12);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(75, 23);
+            this.btnCopy.TabIndex = 1;
+            this.btnCopy.Text = "&Copy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // btnPaste
+            // 
+            this.btnPaste.Location = new System.Drawing.Point(93, 12);
+            this.btnPaste.Name = "btnPaste";
+            this.btnPaste.Size = new System.Drawing.Size(75, 23);
+            this.btnPaste.TabIndex = 2;
+            this.btnPaste.Text = "&Paste";
+            this.btnPaste.UseVisualStyleBackColor = true;
+            this.btnPaste.Click += new System.EventHandler(this.btnPaste_Click);
+            // 
+            // btnSwap
+            // 
+            this.btnSwap.Location = new System.Drawing.Point(174, 12);
+            this.btnSwap.Name = "btnSwap";
+            this.btnSwap.Size = new System.Drawing.Size(75, 23);
+            this.btnSwap.TabIndex = 3;
+            this.btnSwap.Text = "&Swap Case";
+            this.btnSwap.UseVisualStyleBackColor = true;
+            this.btnSwap.Click += new System.EventHandler(this.btnSwap_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSwap);
+            this.Controls.Add(this.btnPaste);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Text Case Swapper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.Button btnPaste;
+        private System.Windows.Forms.Button btnSwap;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CaseSwapper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox1.Text)) return;
+
+            try
+            {
+                Clipboard.SetText(textBox1.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this,
+                    $"Error copying text to the clipboard.  Press Control-C to copy this error so it can be pasted to Reddit.\r\n{ex}",
+                    "Error");
+            }
+        }
+
+        private void btnPaste_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!Clipboard.ContainsText())
+                {
+                    MessageBox.Show(this, "Couldn't find any text in the clipboard.", "Unable to Paste");
+                    return;
+                }
+
+                if (string.IsNullOrWhiteSpace(textBox1.Text) || MessageBox.Show(this,
+                        "Text box is not empty. Replace text with clipboard contents?\r\n\r\nYes: Overwrite existing contents with clipboard\r\nNo: Paste text where cursor is",
+                        "Overwrite", MessageBoxButtons.YesNo, MessageBoxIcon.Question,
+                        MessageBoxDefaultButton.Button2) == DialogResult.Yes)
+                {
+                    textBox1.Text = Clipboard.GetText();
+                }
+                else
+                {
+                    textBox1.Text = string.Concat(textBox1.Text.Substring(0, textBox1.SelectionStart),
+                        Clipboard.GetText(),
+                        textBox1.Text.Substring(textBox1.SelectionStart + textBox1.SelectionLength));
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this,
+                    $"Error reading text from the clipboard.  Press Control-C to copy this error so it can be pasted to Reddit.\r\n{ex}",
+                    "Error");
+            }
+        }
+
+        private void btnSwap_Click(object sender, EventArgs e)
+        {
+            var sb = new StringBuilder();
+
+            foreach (var c in textBox1.Text)
+            {
+                if (!char.IsLetter(c)) {
+                    sb.Append(c);
+                    continue;
+                }
+
+                if (char.IsUpper(c))
+                {
+                    sb.Append((char) (c + 'a' - 'A'));
+                    continue;
+                }
+
+                sb.Append((char) (c + 'A' - 'a'));
+            }
+
+            textBox1.Text = sb.ToString();
+        }
+    }
+}
